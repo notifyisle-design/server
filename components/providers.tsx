504ad585431d7f7007f5bf054477ef0fb44e8599
notifyisle-design <notifyisle@gmail.com>
2026-06-04 08:32:59 +0900

    Replace Next app with standalone HTML prototype

--- components/providers.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import {NextUIProvider} from '@nextui-org/react';
-import {ReactNode} from 'react';
-
-export function Providers({children}: {children: ReactNode}) {
-  return <NextUIProvider>{children}</NextUIProvider>;
-}
